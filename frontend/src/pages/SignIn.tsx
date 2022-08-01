8a0bb977acf0fba6fd02b29a502991ddba2a8c84
Sotatek-SonNguyen9 <69808662+bjergsen243@users.noreply.github.com>
2022-08-01 16:11:12 +0700

    sign-in page, sign-up page not done

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,19 +1,40 @@
-import React from "react"
 import Button from '@mui/material/Button';
 import { blue } from "@mui/material/colors"
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
+import React from 'react';
+import InputAdornment from '@mui/material/InputAdornment';
 
 export default function SignIn() {
+    const [values, setValues] = React.useState({
+        password: '',
+        showPassword: false,
+        
+      });
+      const handleChange = (prop: any) => (event: { target: { value: any; }; }) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+      };
     return(
         <div 
             className="signIn-page" 
             style={{
                 display: "flex",
-                backgroundColor: "green",
+                backgroundColor: "#E58F7E",
                 flexDirection:"row",
+                height: "100vh",
                 // backgroundImage: url() -> error chua them duoc background la anh
             }}
         >
@@ -22,9 +43,15 @@ export default function SignIn() {
                     flex: 1,
                 }}
             >
-                <h1>
-                    Login Page Theme Side
-                </h1>
+                <img 
+                    src="https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg" 
+                    alt="background" 
+                    style={{
+                        objectFit: "contain",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                    }}
+                />
             </div>
             <div 
                 style={{
@@ -43,73 +70,103 @@ export default function SignIn() {
                         marginBottom: "77px"
                     }}
                     variant="outlined"
-                > 
-                    <Typography fontSize={39}>
-                        Sign In
-                    </Typography>
-                    <Typography 
-                        fontSize={14} 
-                        color={blue}
-                    >
-                        Don't have an account?{" "}
-                    <Link 
-                        href="https://facebook.com" 
-                        target={"_blank"}
-                        underline="none"
-                    >
-                        Sign up
-                    </Link>
-                    </Typography>
-                    <br />
-                    <Typography fontSize={14}>
-                        Email Address
-                    </Typography>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        id="email"
-                        label="adrian@gmail.com"
-                        name="email"
-                        autoFocus
-                    />
-                    <br />
-                    <br />
-                    <Typography fontSize={14}>
-                        Password
-                    </Typography>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        name="password"
-                        label="*******"
-                        type="password"
-                        id="password"
-                    />
-                    <br></br>
-                    <br></br>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        style=
-                        {{
-                            backgroundColor: "#C18FF5"
+                >
+                    <div
+                        style={{
+                            alignItems: "center",
+                            margin: "35px",
                         }}
                     >
-                        Sign In
-                    </Button>
-                    <br></br>
-                    <br></br>
-                    <Typography fontSize={14}>
-                        <Link
-                            href="https://google.com"
+                        <Typography 
+                            fontSize={39}
+                            style={{
+                                alignItems: "center",
+                                fontWeight: "600",
+                            }}
+                        >
+                            Sign In
+                        </Typography>
+                        <Typography 
+                            fontSize={14} 
+                            color={blue}
+                        >
+                            Don't have an account?{" "}
+                        <Link 
+                            href="https://facebook.com" 
+                            target={"_blank"}
                             underline="none"
                         >
-                            Forgot your password?
+                            Sign up
                         </Link>
-                    </Typography>
+                        </Typography>
+                        <br />
+                        <Typography fontSize={14} fontWeight={600}>
+                            Email Address
+                        </Typography>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            required
+                            id="email"
+                            label="adrian@gmail.com"
+                            name="email"
+                            autoFocus
+                        />
+                        <br />
+                        <br />
+                        <Typography fontSize={14} fontWeight={600}>
+                            Password
+                        </Typography>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            required
+                            name="password"
+                            label="*******"
+                            id="password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            InputProps={{
+                                endAdornment:   
+                                    <InputAdornment position="end"> Show password
+                                        {/* <IconButton
+                                            //onClick={handleClickShowPassword}
+                                            //onMouseDown={handleMouseDownPassword}
+                                            //edge="end"
+                                        >
+                                           //{values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton> */}
+                                    </InputAdornment>  
+                            }}
+                        />
+                        <br></br>
+                        <br></br>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            style=
+                            {{
+                                backgroundColor: "#C18FF5",
+                                textTransform: "none"
+                            }}
+                        >
+                            Sign In
+                        </Button>
+                        <br></br>
+                        <br></br>
+                        <Typography fontSize={14}>
+                            <Link
+                                href="https://google.com"
+                                underline="none"
+                                fontWeight={600}
+                            >
+                                Forgot your password?
+                            </Link>
+                        </Typography>
+                    </div>
+                    
                 </Paper>
                 
             </div>
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,20 +1,45 @@
 import Paper from "@mui/material/Paper"
 import Typography from "@mui/material/Typography"
 import Link from "@mui/material/Link";
-import React from "react"
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 
+import { useState } from "react";
+import { IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import React from "react";
+
 export default function SignUp() {
+    const [values, setValues] = React.useState({
+        password: '',
+        showPassword: false,
+        currentPassword: '',
+        showCurrentPassword: false
+      });
+      const handleChange = (prop: any) => (event: { target: { value: any; }; }) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+      };
     return(
         <div 
             className="signUp-page" 
             style={{
                 display: "flex",
-                flexDirection: "row",
-                backgroundColor: "yellow"
+                backgroundColor: "#E58F7E",
+                flexDirection:"row",
+                height: "100vh",
             }}
         >
             <div
@@ -22,9 +47,15 @@ export default function SignUp() {
                     flex: 1,
                 }}
             >
-                <h1>
-                    Sign Up Theme Side
-                </h1>
+                <img 
+                    src="https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg" 
+                    alt="background" 
+                    style={{
+                        objectFit: "contain",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                    }}
+                />
             </div>
             <div
                 style={{
@@ -44,15 +75,22 @@ export default function SignUp() {
                     }}
                     variant="outlined"
                 >
-                    <Typography
-                        fontSize={39}
+                    <div
+                        style={{
+                            alignItems: "center",
+                            margin: "35px",
+                        }}
                     >
-                        Sign Up
-                    </Typography>
-                    <Typography
-                        fontSize={14}
-                    >
-                        Have an account?{" "} 
+                        <Typography
+                            fontSize={39}
+                            style={{
+                                alignItems: "center",
+                                fontWeight: "600",
+                            }}
+                        >
+                            Sign Up
+                        </Typography>
+                        <Typography fontSize={14}> Have an account?{" "} 
                             <Link 
                                 href="https://google.com"
                                 underline="none"
@@ -60,51 +98,82 @@ export default function SignUp() {
                             >
                                 Sign in
                             </Link>
-                    </Typography>
-                    <br />
-                    <Typography fontSize={14}> Email Address </Typography>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        id="email"
-                        label=""
-                        autoFocus
-                    />
-                    <Typography fontSize={14}> Password </Typography>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        id="password"
-                        label=""
-                        autoFocus
-                    />
-                    <Typography fontSize={14}> Confirm Password </Typography>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        id="confirm-password"
-                        label=""
-                        autoFocus
-                    />             
-                    <FormControlLabel 
-                        control={<Checkbox defaultChecked/>} 
-                        label={ <Typography fontSize={14}>I accept the{" "}
-                                    <Link>Term & Conditions</Link>
-                                </Typography>}></FormControlLabel>     
-                    <Button 
-                         type="submit"
-                         fullWidth
-                         variant="contained"
-                         style=
-                         {{
-                             backgroundColor: "#C18FF5"
-                         }}
-                    >   
-                        Create Account
-                    </Button>
+                        </Typography>
+                        <br />
+                        <Typography fontSize={14} fontWeight={600}> Email Address </Typography>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            required
+                            id="email"
+                            label=""
+                            autoFocus
+                        />
+                        <Typography fontSize={14} fontWeight={600}> Password </Typography>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            required
+                            id="password"
+                            autoFocus
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            InputProps={{
+                                endAdornment:   
+                                    <InputAdornment position="end"> Show password
+                                        {/* <IconButton
+                                            //onClick={handleClickShowPassword}
+                                            //onMouseDown={handleMouseDownPassword}
+                                            //edge="end"
+                                        >
+                                           //{values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton> */}
+                                    </InputAdornment>  
+                            }}
+                        />
+                        <Typography fontSize={14} fontWeight={600}> Confirm Password </Typography>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            required
+                            id="confirm-password"
+                            autoFocus
+                            type={values.showCurrentPassword ? 'text' : 'password'}
+                            value={values.currentPassword}
+                            onChange={handleChange('currentPassword')}
+                            InputProps={{
+                                endAdornment:   
+                                    <InputAdornment position="end"> Show current password
+                                        {/* <IconButton
+                                            //onClick={handleClickShowPassword}
+                                            //onMouseDown={handleMouseDownPassword}
+                                            //edge="end"
+                                        >
+                                           //{values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton> */}
+                                    </InputAdornment>  
+                            }}
+                        />             
+                        <FormControlLabel 
+                            control={<Checkbox defaultChecked/>} 
+                            label={ <Typography fontSize={14}>I accept the{" "}
+                                        <Link underline="none">Terms & Conditions.</Link>
+                                    </Typography>}></FormControlLabel>     
+                        <Button 
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            style=
+                            {{
+                                backgroundColor: "#C18FF5",
+                                fontWeight: "700",
+                                textTransform: "none"
+                            }}
+                        >   
+                            Create Account
+                        </Button>
+                    </div>
                 </Paper>
             </div>
         </div>
