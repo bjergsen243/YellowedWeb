51e2a85af4e482e055e0a40a5f164acd03f27f28
Sotatek-SonNguyen9 <69808662+bjergsen243@users.noreply.github.com>
2022-08-03 15:37:12 +0700

    cac thanh phan trong TabBar

---
+++ frontend/src/components/TabBarComponent.tsx
@@ -0,0 +1,28 @@
+import {Button, Link, SvgIcon} from "@mui/material";
+import {ReactComponent as IconHomePage} from "../assets/icon-home-page.svg";
+import {ReactComponentElement} from "react";
+interface ITabBarFeatureComponent {
+  div_id: string;
+  // componentIcon: string;
+  hrefLink: string;
+  featureDisplay: string;
+}
+export default function TabBarComponent({div_id, hrefLink, featureDisplay}: ITabBarFeatureComponent) {
+  return(
+      <div id={div_id}>
+        <Button>
+          <SvgIcon component={IconHomePage} inheritViewBox/>
+        </Button>
+        <Link
+            underline="none"
+            href={hrefLink}
+            color="inherit"
+            style={{
+              fontWeight: 600,
+            }}
+        >
+          {featureDisplay}
+        </Link>
+      </div>
+  )
+}
